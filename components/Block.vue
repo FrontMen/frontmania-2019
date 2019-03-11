d20d72de22c424161c6af0b8a4b792a5ef055e70
LC <lassecapel@gmail.com>
2019-03-11 10:39:14 +0100

    implement new events design

---
+++ components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="fm-block">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FmBlock'
+}
+</script>
+
+<style scoped lang="scss">
+.fm-block {
+  position: relative;
+  padding: 45px;
+  background-color: black;
+  color: #fff;
+
+  @include media('>=tablet') {
+    padding: 65px;
+  }
+}
+</style>
---
+++ components/Row.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="fm-row">
+    <h2 class="section-title">{{event}}</h2>
+    <div class="section-divider--themed"></div>
+    <div class="section-row">
+      <div>{{date}}</div>
+      <div>{{venue}}</div>
+      <div>{{location}}</div>
+      <FmBtn>button</FmBtn>
+    </div>
+    <div class="section-divider"></div>
+  </div>
+</template>
+
+<script>
+import FmBtn from '~/components/Button.vue'
+export default {
+  name: 'FmBlock',
+  components: {
+    FmBtn
+  },
+  props: ['event', 'date', 'venue', 'location']
+}
+</script>
+
+<style scoped lang="scss">
+.fm-row {
+  margin-bottom: 28px;
+}
+.section-title {
+  text-transform: uppercase;
+  color: #ffed00;
+  margin-bottom: 11px;
+}
+.section-divider {
+  width: 100%;
+  border-bottom: 1px solid #ffffff;
+  &--themed {
+    border-color: #ffed00;
+  }
+}
+.section-divider--themed {
+  width: 100%;
+  border-bottom: 1px solid #ffed00;
+}
+
+.section-row {
+  display: flex;
+  justify-content: space-between;
+  width: 600px;
+  margin: 28px 0;
+}
+</style>
