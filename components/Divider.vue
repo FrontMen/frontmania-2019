3116841ca277bfa6791d66a29f196f0cc93fbbfb
Prashant Palikhe <prashpal09@gmail.com>
2019-04-09 21:21:25 +0200

    feat: Drawer with Tailwind

---
+++ components/Divider.vue
@@ -0,0 +1,14 @@
+<template functional>
+  <span class="divider" />
+</template>
+
+
+<style scoped lang="scss">
+.divider {
+  display: block;
+  height: 8px;
+  width: 64px;
+  background: #fff;
+  margin-bottom: 10px;
+}
+</style>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="fm-sidebar">
+  <div class=" fm-sidebar fixed w-90 h-screen black p-8 pt-12 text-center">
     <nuxt-link to="/">
       <fm-logo />
     </nuxt-link>
 
-    <div class="fm-sidebar__info">
-      <p>FrontMania is a group of people who are crazy about frontend development a.k.a. frontend maniacs.</p>
-      <p>We as a group believe that frontend development should be fun for everyone!</p>
+    <div class="fm-sidebar__info absolute pin-l white--text text-left px-8">
+      <p class="mb-6 text-base">FrontMania is a group of people who are crazy about frontend development a.k.a. frontend maniacs.</p>
+      <p class="mb-6 text-base">We as a group believe that frontend development should be fun for everyone!</p>
 
-      <span class="divider" />
-      <nuxt-link to="/privacy" class="link__privacy">
+      <fm-divider />
+
+      <nuxt-link to="/privacy" class="yellow--text text-base">
         PRIVACY POLICY
       </nuxt-link>
     </div>
@@ -18,60 +19,16 @@
 
 <script>
 import FmLogo from '@/components/Logo'
+import FmDivider from '@/components/Divider'
 
 export default {
   name: 'FmSidebar',
-  components: { FmLogo }
+  components: { FmLogo, FmDivider }
 }
 </script>
 
 <style scoped lang="scss">
-.fm-sidebar {
-  position: relative;
-  width: 100%;
-  background-color: black;
-  text-align: center;
-  padding: 50px 0;
-
-  @include media('>=tablet') {
-    position: fixed;
-    width: 360px;
-    height: 100vh;
-  }
-}
-
 .fm-sidebar__info {
-  color: white;
-  padding: 0 32px;
   bottom: 50px;
-  text-align: center;
-  margin-top: 50px;
-
-  @include media('>=tablet') {
-    text-align: left;
-    position: absolute;
-  }
-
-  p {
-    margin-bottom: 24px;
-  }
-
-  .divider {
-    display: block;
-    height: 8px;
-    width: 64px;
-    background: #fff;
-    margin-bottom: 10px;
-  }
-
-  a {
-    color: #ffed00;
-  }
-
-  p,
-  a {
-    font: normal 300 16px/1.5 GTAmericaMono;
-    letter-spacing: 1px;
-  }
 }
 </style>
