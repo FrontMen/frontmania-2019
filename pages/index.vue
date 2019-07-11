c3f0cc05783d052da3602d3f6bd7c1490cb18cd6
jordanmauricio <jordanmauricio1@gmail.com>
2019-07-11 16:29:04 +0200

    added graffiti paint on canvas

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="lg:flex items-center justify-center lg:h-screen">
+  <section id="canvas-wrapper" class="lg:flex items-center justify-center lg:h-screen">
     <app-slider />
 
     <app-toolbar>
@@ -29,6 +29,7 @@
         </div>
       </app-block>
     </div>
+    <canvas id="c" width="800" height="600"></canvas>
   </section>
 </template>
 
@@ -55,6 +56,77 @@ export default {
     }))
 
     return { events }
+  },
+  mounted() {
+    const el = document.getElementById('c')
+    const container = document.querySelector('#canvas-wrapper')
+    const canvasWidth = window.innerWidth - 1
+    const canvasHeight = window.innerHeight - 1
+    el.width = canvasWidth - el.offsetLeft
+    el.height = canvasHeight
+    const ctx = el.getContext('2d')
+    let clientX, clientY, timeout
+    const density = 25
+
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max))
+    }
+
+    function getRandomFloat(min, max) {
+      return Math.random() * (max - min) + min
+    }
+
+    container.onmousemove = function(e) {
+      ctx.lineJoin = ctx.lineCap = 'round'
+      clientX = e.clientX - el.offsetLeft
+      clientY = e.clientY
+
+      timeout = setTimeout(function draw() {
+        for (let i = density; i--; ) {
+          const angle = getRandomFloat(0, Math.PI * 2)
+          const radius = getRandomFloat(0, 40)
+          ctx.fillStyle = '#ffed00'
+          ctx.shadowColor = '#fff78c'
+          ctx.shadowBlur = 10
+          ctx.fillRect(
+            clientX + radius * Math.cos(angle),
+            clientY + radius * Math.sin(angle),
+            getRandomInt(3),
+            getRandomInt(3)
+          )
+        }
+        if (!timeout) return
+        timeout = setTimeout(draw, 50)
+      }, 50)
+    }
+    container.onmouseleave = function() {
+      clearTimeout(timeout)
+    }
   }
 }
 </script>
+
+<style>
+#c {
+  top: 0;
+  z-index: -1;
+}
+
+.content {
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  /* color: #f9f1e9; */
+  content: #000;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate3d(-50%, -50%, 0);
+  transform: translate3d(-50%, -50%, 0);
+}
+
+.testing-header {
+  font-size: 6em;
+  letter-spacing: 0.1em;
+}
+</style>
