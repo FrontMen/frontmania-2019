f50af99d1c3ba72c08ca028eb2a17b5b0e308f40
Janine Amelie Lourens <janine.lourens@frontmen.nl>
2019-09-19 13:53:06 +0200

    feat: add Team boilerplate

---
+++ components/Conference/SectionTeam.vue
@@ -0,0 +1,20 @@
+<template>
+  <app-section class="background" background="/images/bg-pink.jpg">
+    <div class="bg-app-yellow flex flex-wrap items-center p-4 sm:p-8">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolor placeat quod tenetur, odio, mollitia esse consectetur iure assumenda suscipit ab pariatur, quisquam adipisci? Eum a nesciunt deserunt temporibus facilis!
+    </div>
+  </app-section>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+</style>
