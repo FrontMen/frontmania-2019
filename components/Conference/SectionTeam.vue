f1cc97c280fe7d684ddb7a0e3f870e2e518947f0
Janine Amelie Lourens <janine.lourens@frontmen.nl>
2019-09-19 16:40:56 +0200

    feat: finished team page

--- components/Conference/SectionTeam.vue
+++ components/Conference/SectionTeam.vue
@@ -1,18 +1,75 @@
 <template>
-  <app-section class="background" background="/images/bg-pink.jpg">
-    <div class="bg-app-yellow flex flex-wrap items-center p-4 sm:p-8">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolor placeat quod tenetur, odio, mollitia esse consectetur iure assumenda suscipit ab pariatur, quisquam adipisci? Eum a nesciunt deserunt temporibus facilis!
+  <app-section class="background" background="/images/bg-yellow.jpg">
+    <div class="bg-app-white">
+      <div class="flex flex-wrap items-center">
+        <div class="w-full xl:w-1/2 sm:text-center mb-4 px-6 pt-8">
+          <img src="~/assets/images/conference/team.svg">
+        </div>
+
+        <div class="w-full xl:w-1/2 p-4 sm:p-8 xl:pl-0 xl:pr-4 xl:bg-transparent text-app-black xl:text-black">
+          <p>
+            Meet the awesome people who helped make FrontMania happen! 💪
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-wrap justify-center">
+      <template v-for="member in teamMembers">
+        <TeamMate :key="member.image" v-bind="member" />
+      </template>
     </div>
   </app-section>
 </template>
 
 <script>
+import TeamMate from './TeamMate'
+
 export default {
-  components: {},
-  data() {
-    return {}
+  components: {
+    TeamMate
   },
-  methods: {}
+  data() {
+    return {
+      teamMembers: [
+        {
+          name: 'Randy Daal',
+          subTitle: 'King Manager 👑',
+          image: 'randyDaal',
+          color: 'blue'
+        },
+        {
+          name: 'Lasse Capel',
+          subTitle: 'Spice unicorn 🌶',
+          image: 'lasse',
+          color: 'red'
+        },
+        {
+          name: 'Prashant Palikhe',
+          subTitle: 'Likes Sushi 🍣',
+          image: 'prashant',
+          color: 'pink'
+        },
+        {
+          name: 'Janine Lourens',
+          subTitle: 'Resident Unicorn 🦄',
+          image: 'janine',
+          color: 'pink'
+        },
+        {
+          name: 'Steven Straatemans',
+          subTitle: 'Has glasses 👓',
+          image: 'steven',
+          color: 'green'
+        },
+        {
+          name: 'Stefanos Lignos ',
+          subTitle: 'Watches football ⚽',
+          image: 'stefanos',
+          color: 'blue'
+        }
+      ]
+    }
+  }
 }
 </script>
 
